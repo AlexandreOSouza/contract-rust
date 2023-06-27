553e7489b8afd57953332c97ccd7e706e0a08c79
Alexandre <alexandre.o.souza12@gmail.com>
2023-06-26 21:14:48 -0300

    chore: add some functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{env, near_bindgen};
+
+const PASSWORD_NUMBER: u8 = 1;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contact {
+pub struct Contract {
     // SETUP CONTRACT STATE
+    password_solution: String,
 }
 
 #[near_bindgen]
 impl Contract {
     // ADD CONTRACT METHODS HERE
+    pub fn get_password_number(&self) -> u8 {
+        PASSWORD_NUMBER
+    }
+
+    pub fn set_solution(&mut self, solution: String) {
+        self.password_solution = solution;
+    }
+
+    pub fn guess_solution(&mut self, solution: String) {
+        if solution == self.password_solution {
+            env::log_str("You may enter! This is the right password");
+        } else {
+            env::log_str("You may enter! This is the wrong password");
+        }
+    }
 }
 
 #[cfg(test)]
